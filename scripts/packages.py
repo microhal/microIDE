75432eaa31c222338742ef31cab1ec6bb8c8ed13
sakoPO <sako.po@gmail.com>
2018-08-30 23:57:21 +0200

    Updated packages list

--- scripts/packages.py
+++ scripts/packages.py
@@ -3,7 +3,8 @@ toolchains = {
         'gcc-arm-none-eabi_5_3-2016q1-20160330': {
             'linux': {
                 'filename': 'gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '5.3.0',
                 'url': 'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2',
                 'checksum': {'md5': '5a261cac18c62d8b7e8c70beba2004bd'},
@@ -12,17 +13,20 @@ toolchains = {
             },
             'windows': {
                 'filename': 'gcc-arm-none-eabi-5_3-2016q1-20160330-win32.exe',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '5.3.0',
                 'url': 'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-win32.exe',
                 'checksum': {'': ''},
                 'licenseUrl': 'https://launchpadlibrarian.net/251686212/license.txt',
-                'installationLocation': '{app}\\toolchains\\gcc-arm-none-eabi\\microhal'}
+                'installationLocation': '{app}\\toolchains\\gcc-arm-none-eabi\\microhal'
+            }
         },
         'gcc-arm-none-eabi_6_2016_q2': {
             'linux': {
                 'filename': 'gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '6.2.0',
                 'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-linux.tar.bz2?revision=2cc92fb5-3e0e-402d-9197-bdfc8224d8a5?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,6-2017-q2-update',
                 'checksum': {'md5': '13747255194398ee08b3ba42e40e9465'},
@@ -34,7 +38,8 @@ toolchains = {
         'gcc-arm-none-eabi_7_2017_q4': {
             'linux': {
                 'filename': 'gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.2.0',
                 'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2?revision=375265d4-e9b5-41c8-bf23-56cbe927e156?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,7-2017-q4-major',
                 'checksum': {'md5': 'd3b00ae09e847747ef11316a8b04989a'},
@@ -43,7 +48,8 @@ toolchains = {
             },
             'windows': {
                 'filename': 'gcc-arm-none-eabi-7-2017-q4-major-win32.exe',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.2.0',
                 'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-win32.exe?revision=732bae94-c929-403d-9520-0b2bccd81ad7?product=GNU%20Arm%20Embedded%20Toolchain,32-bit,,Windows,7-2017-q4-major',
                 'checksum': {'md5': 'bb4def39ff1cb3ff5d2931597d9aea4e'},
@@ -54,7 +60,8 @@ toolchains = {
         'gcc-arm-none-eabi-7-2018-q2-update': {
             'linux': {
                 'filename': 'gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.3.0',
                 'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2?revision=bc2c96c0-14b5-4bb4-9f18-bceb4050fee7?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,7-2018-q2-update',
                 'checksum': {'md5': '299ebd3f1c2c90930d28ab82e5d8d6c0'},
@@ -62,13 +69,14 @@ toolchains = {
                 'installationLocation': 'microideDir/toolchains/gcc-arm-none-eabi/microhal'
             },
             'windows': {
-                'filename': 'gcc-arm-none-eabi-7-2018-q2-update-win32-sha2.exe',
-                'size': '0',
+                'filename': 'gcc-arm-none-eabi-7-2018-q2-update-win32.zip',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.3.0',
-                'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-win32-sha2.exe?revision=bc72768f-9927-454e-9141-918927fb74ff?product=GNU%20Arm%20Embedded%20Toolchain,32-bit,,Windows,7-2018-q2-update',
-                'checksum': {'md5': 'f94f652fec26efa90874d7f40cf2ca6c'},
+                'url': 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-win32.zip?revision=9b83e69c-e279-4d16-9401-990c836197b3?product=GNU%20Arm%20Embedded%20Toolchain,ZIP,,Windows,7-2018-q2-update',
+                'checksum': {'md5': ' bc8ae26d7c429f30d583a605a4bcf9bc'},
                 'licenseUrl': 'https://developer.arm.com/GetEula?Id=7f7f1581-ce37-49ab-972f-c4163729fa47',
-                'installationLocation': 'microideDir/toolchains/gcc-arm-none-eabi/microhal'
+                'installationLocation': '{app}\\toolchains\\gcc-arm-none-eabi\\microhal'
             }
         }
     },
@@ -77,7 +85,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'LLVM-3.8.0-win64.exe',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '3.8.0',
                 'url': 'http://llvm.org/releases/3.8.0/LLVM-3.8.0-win64.exe',
                 'checksum': {'': ''},
@@ -89,7 +98,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'LLVM-6.0.0-win64.exe',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '6.0.0',
                 'url': 'http://releases.llvm.org/6.0.0/LLVM-6.0.0-win64.exe',
                 'checksum': {'': ''},
@@ -101,7 +111,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'LLVM-6.0.1-win64.exe',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '6.0.1',
                 'url': 'http://releases.llvm.org/6.0.1/LLVM-6.0.1-win64.exe',
                 'checksum': {'': ''},
@@ -115,7 +126,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'x86_64-7.1.0-release-win32-seh-rt_v5-rev2.7z',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.1.0',
                 'url': 'https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/7.1.0/threads-win32/seh/x86_64-7.1.0-release-win32-seh-rt_v5-rev2.7z',
                 'checksum': {'md5': 'bd537f46793fc11b7b161f071e9ef31e'},
@@ -126,7 +138,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '7.3.0',
                 'url': 'https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/7.3.0/threads-posix/seh/x86_64-7.3.0-release-posix-seh-rt_v5-rev0.7z',
                 'checksum': {'md5': '7240f9d64a06e2c71141380a6db0cf64'},
@@ -138,7 +151,8 @@ toolchains = {
             'linux': {},
             'windows': {
                 'filename': 'x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z',
-                'size': '0',
+                'download size': '0',
+                'installation size': '0',
                 'version': '8.1.0',
                 'url': 'https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/8.1.0/threads-posix/seh/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z',
                 'checksum': {'md5': '55c00ca779471df6faf1c9320e49b5a9'},
@@ -152,7 +166,8 @@ toolchains = {
 openOCD = {
     'linux': {
         'filename': 'openocd-0.10.0.tar.gz',
-        'size': '0',
+        'download size': '0',
+        'installation size': '0',
         'version': '0.10.0',
         'url': 'https://sourceforge.net/projects/openocd/files/openocd/0.10.0/openocd-0.10.0.tar.gz/download',
         'checksum': {'md5': '8971d16aee5c2642b33ee55fc6c86239'},
@@ -160,7 +175,8 @@ openOCD = {
     },
     'windows': {
         'filename': 'openocd-0.10.0.7z',
-        'size': '0',
+        'download size': '0',
+        'installation size': '0',
         'version': '0.10.0',
         'url': 'http://www.freddiechopin.info/en/download/category/4-openocd?download=154%3Aopenocd-0.10.0',
         'checksum': {'SHA256': 'f46687cd783a7a86716c78474e8132e32de84b773914f23f2226f81509ffcfca'},
@@ -171,7 +187,8 @@ openOCD = {
 eclipse = {
     'linux': {
         'filename': 'eclipse-inst-linux64.tar.gz',
-        'size': '0',
+        'download size': '0',
+        'installation size': '0',
         'version': 'oxygen',
         'url': 'http://www.eclipse.org/downloads/download.php?file=/oomph/products/latest/eclipse-inst-linux64.tar.gz\&r=1',
         'checksum': {'': ''},
@@ -179,7 +196,8 @@ eclipse = {
     },
     'windows': {
         'filename': 'eclipse-inst-win64.exe',
-        'size': '0',
+        'download size': '0',
+        'installation size': '0',
         'version': 'oxygen',
         'url': 'http://www.eclipse.org/downloads/download.php?file=/oomph/products/latest/eclipse-inst-win64.exe\&r=1',
         'checksum': {'': ''},
@@ -192,7 +210,8 @@ doxygen = {
         'linux': {},
         'windows': {
             'filename': 'doxygen-1.8.13-setup.exe',
-            'size': '0',
+            'download size': '0',
+            'installation size': '0',
             'version': '1.8.13',
             'url': 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13-setup.exe',
             'checksum': {'': ''},
@@ -204,7 +223,8 @@ doxygen = {
         'linux': {},
         'windows': {
             'filename': 'doxygen-1.8.14.windows.x64.bin.zip',
-            'size': '0',
+            'download size': '0',
+            'installation size': '0',
             'version': '1.8.14',
             'url': 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.windows.x64.bin.zip',
             'checksum': {'': ''},
@@ -219,7 +239,8 @@ cppcheck = {
         'linux': {},
         'windows': {
             'filename': 'cppcheck-1.83-x64-Setup.msi',
-            'size': '0',
+            'download size': '0',
+            'installation size': '0',
             'version': '1.83',
             'url': 'https://github.com/danmar/cppcheck/releases/download/1.83/cppcheck-1.83-x64-Setup.msi',
             'checksum': {'': ''},
@@ -231,12 +252,43 @@ cppcheck = {
         'linux': {},
         'windows': {
             'filename': 'cppcheck-1.84-x64-Setup.msi',
-            'size': '0',
+            'download size': '0',
+            'installation size': '0',
             'version': '1.84',
             'url': 'http://github.com/danmar/cppcheck/releases/download/1.84/cppcheck-1.84-x64-Setup.msi',
             'checksum': {'': ''},
             'licenseUrl': 'https://github.com/danmar/cppcheck/blob/master/COPYING',
             'installationLocation': '{app}\\tools\\cppcheck\\1.84'
+        }
+    }
+}
+
+graphviz = {
+    '2.38': {
+        'linux': {},
+        'windows': {
+            'filename': 'graphviz-2.38.zip',
+            'download size': '0',
+            'installation size': '0',
+            'version': '2.38',
+            'url': 'https://graphviz.gitlab.io/_pages/Download/windows/graphviz-2.38.zip',
+            'checksum': {'': ''},
+            'licenseUrl': '',
+            'installationLocation': '{app}\\tools\\graphviz\\2.38'}
+    }
+}
+
+msys = {
+    '13': {
+        'windows': {
+            'filename': 'msys-rev13.7z',
+            'download size': '0',
+            'installation size': '0',
+            'version': '2.38',
+            'url': 'http://downloads.sourceforge.net/project/mingwbuilds/external-binary-packages/msys%2B7za%2Bwget%2Bsvn%2Bgit%2Bmercurial%2Bcvs-rev13.7z',
+            'checksum': {'md5': '4c79f989eb6353a0d81bc39b6f7176ea'},
+            'licenseUrl': '',
+            'installationLocation': '{app}\\tools\\msys'
         }
     }
 }
